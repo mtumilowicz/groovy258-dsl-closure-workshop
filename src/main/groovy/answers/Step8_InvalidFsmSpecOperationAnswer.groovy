4f8dfe43292c7ddc996b6b3d9544149620cddac6
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-10-30 19:48:01 +0100

    exceptions: naming

--- src/main/groovy/answers/Step8_InvalidFsmSpecOperationAnswer.groovy
+++ src/main/groovy/answers/Step8_InvalidFsmSpecOperationAnswer.groovy
@@ -1,7 +1,7 @@
 package answers
 
 class Step8_InvalidFsmSpecOperationAnswer extends RuntimeException {
-    Step8_InvalidFsmSpecOperationAnswer(String message) {
-        super("Operation: ${message} is invalid according to fsm specification")
+    Step8_InvalidFsmSpecOperationAnswer(String operation) {
+        super("Operation: ${operation} is invalid according to fsm specification")
     }
 }
